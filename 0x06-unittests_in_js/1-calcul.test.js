444536016d8aa312edfbd15c3e0511d618a82b19
grillzwitu <grillzwitu@gmail.com>
2022-01-27 15:54:21 +0100

    implemented tests with mocha

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,94 +2,46 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', function () {
-    describe('sum', function () {
-      it('should be 1 + 5 = 6', function () {
-        assert.equal(calculateNumber('SUM', 1, 5), 6);
-      });
-      it('should be -1 + 5 = 4', function () {
-        assert.equal(calculateNumber('SUM', -1, 5), 4);
-      });
-      it('should be 1.3 + -5.7 = -5', function () {
-        assert.equal(calculateNumber('SUM', 1.3, -5.7), -5);
-      });
-      it('should be -1.8 + -5.1 = -7', function () {
-        assert.equal(calculateNumber('SUM', -1.8, -5.1), -7);
-      });
+  describe('sum', function () {
+    it('should be 2 + 4 = 6', function () {
+      assert.equal(calculateNumber('SUM', 2, 4), 6);
     });
-    describe('subtract', function () {
-      it('should be 1 - 5 = -4', function () {
-        assert.equal(calculateNumber('SUBTRACT', 1, 5), -4);
-      });
-      it('should be -1 - 5 = -6', function () {
-        assert.equal(calculateNumber('SUBTRACT', -1, 5), -6);
-      });
-      it('should be 1.3 - -5.7 = 7', function () {
-        assert.equal(calculateNumber('SUBTRACT', 1.3, -5.7), 7);
-      });
-      it('should be -1.8 - -5.1 = 3', function () {
-        assert.equal(calculateNumber('SUBTRACT', -1.8, -5.1), 3);
-      });
+    it('should be -2 + -5 = -7', function () {
+      assert.equal(calculateNumber('SUM', -2, -5), -7);
     });
-    describe('divide', function () {
-      it('should be 1 / 5 = 0.2', function () {
-        assert.equal(calculateNumber('DIVIDE', 1, 5), 0.2);
-      });
-      it('should be -1 / 5 = -0.2', function () {
-        assert.equal(calculateNumber('DIVIDE', -1, 5), -0.2);
-      });
-      it('should be 1.3 / -4.7 = -5', function () {
-        assert.equal(calculateNumber('DIVIDE', 1.3, -4.7), -0.2);
-      });
-      it('should be -1.8 / -5.1 = -7', function () {
-        assert.equal(calculateNumber('DIVIDE', -1.8, -5.1), 0.4);
-      });
-      it('should be -1.8 / -5.1 = -7', function () {
-        assert.equal(calculateNumber('DIVIDE', -1.8, 0), 'Error');
-      });
+    it('should be 1.3 + -5.7 = -5', function () {
+      assert.equal(calculateNumber('SUM', 1.3, -5.7), -5);
+    });
+    it('should be -1 + 5 = 4', function () {
+      assert.equal(calculateNumber('SUM', -1, 5), 4);
     });
   });
-
-// describe('calculateNumber', function () {
-//   describe('sum', function () {
-//     it('should be 2 + 4 = 6', function () {
-//       assert.equal(calculateNumber('SUM', 2, 4), 6);
-//     });
-//     it('should be -2 + -5 = -7', function () {
-//       assert.equal(calculateNumber('SUM', -2, -5), -7);
-//     });
-//     it('should be 1.3 + -5.7 = -5', function () {
-//       assert.equal(calculateNumber('SUM', 1.3, -5.7), -5);
-//     });
-//     it('should be -1 + 5 = 4', function () {
-//       assert.equal(calculateNumber('SUM', -1, 5), 4);
-//     });
-//   });
-//   describe('subtract', function () {
-//     it('should be 2 - 4 = -2', function () {
-//       assert.equal(calculateNumber('SUBTRACT', 2, 4), -2);
-//     });
-//     it('should be -2 - -5 = 3', function () {
-//       assert.equal(calculateNumber('SUBTRACT', -2, -5), 3);
-//     });
-//     it('should be 1.3 - -5.7 = 7', function () {
-//       assert.equal(calculateNumber('SUBTRACT', 1.3, -5.7), 7);
-//     });
-//     it('should be -1 - 5 = -6', function () {
-//       assert.equal(calculateNumber('SUBTRACT', -1, 5), -6);
-//     });
-//   });
-//   describe('divide', function () {
-//     it('should be 2 / 4 = 0.5', function () {
-//       assert.equal(calculateNumber('DIVIDE', 2, 4), 0.5);
-//     });
-//     it('should be 1.3 / -4.7 = -0.2', function () {
-//         assert.equal(calculateNumber('DIVIDE', 1.3, -4.7), -0.2);
-//       });
-//     it('should be -2 / -5 = 0.4', function () {
-//       assert.equal(calculateNumber('DIVIDE', -2, -5), 0.4);
-//     });
-//     it('should be -1.5 / 0 = Error', function () {
-//       assert.equal(calculateNumber('DIVIDE', -1.5, 0), 'Error');
-//     });
-//   });
-// });
+  describe('subtract', function () {
+    it('should be 2 - 4 = -2', function () {
+      assert.equal(calculateNumber('SUBTRACT', 2, 4), -2);
+    });
+    it('should be -2 - -5 = 3', function () {
+      assert.equal(calculateNumber('SUBTRACT', -2, -5), 3);
+    });
+    it('should be 1.3 - -5.7 = 7', function () {
+      assert.equal(calculateNumber('SUBTRACT', 1.3, -5.7), 7);
+    });
+    it('should be -1 - 5 = -6', function () {
+      assert.equal(calculateNumber('SUBTRACT', -1, 5), -6);
+    });
+  });
+  describe('divide', function () {
+    it('should be 2 / 4 = 0.5', function () {
+      assert.equal(calculateNumber('DIVIDE', 2, 4), 0.5);
+    });
+    it('should be 1.3 / -4.7 = -0.2', function () {
+        assert.equal(calculateNumber('DIVIDE', 1.3, -4.7), -0.2);
+      });
+    it('should be -2 / -5 = 0.4', function () {
+      assert.equal(calculateNumber('DIVIDE', -2, -5), 0.4);
+    });
+    it('should be -1.5 / 0 = Error', function () {
+      assert.equal(calculateNumber('DIVIDE', -1.5, 0), 'Error');
+    });
+  });
+});
